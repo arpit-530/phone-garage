8049f503d5c23731edfdf336b495735acec5e2f6
arpitg530 <95230806+arpitg530@users.noreply.github.com>
2022-11-30 15:11:07 +0530

    Add files via upload

---
+++ phone-garage/src/main/java/com/pgm/phonegarage/PhoneGarageApplication.java
@@ -0,0 +1,13 @@
+package com.pgm.phonegarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneGarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneGarageApplication.class, args);
+	}
+
+}
---
+++ phone-garage/src/test/java/com/pgm/phonegarage/PhoneGarageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgm.phonegarage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneGarageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
